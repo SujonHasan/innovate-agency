f3e0d096912525de3023a5899781be58c6e0e64c
SujonHasan <sujonhasan171@gmail.com>
2024-10-08 22:45:05 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="font-bold text-5xl text-red-700" >Hello Programmer </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
